2e57303dc04b0d786618d5c5c94d06eb71ffbdc1
Celestinvib <celestinvib@gmail.com>
2022-08-20 11:31:41 +0200

    deploy-heroku-ex2 v1.0

--- TA25_Exercise1/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into manufacturers (name) values('MSI');
-insert into manufacturers (name) values('Intel');
-
-insert into items (name,price,manufacturer_code) values('Intel i5 8400 2.4Ghz',199.99,2);
-insert into items (name,price,manufacturer_code) values('Tarjeta gráfica GTX1060 3GB ',209.99,1);
--- TA25_Exercise4/src/main/java/com/crud/h2/dao/IMovieDAO.java
+++
@@ -1,9 +0,0 @@
-package com.crud.h2.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crud.h2.dto.Movie;
-
-public interface IMovieDAO extends JpaRepository<Movie, Long>{
-
-}
