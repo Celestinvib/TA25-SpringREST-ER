e338859291386031e34ff9d3599a8a7b54a8cb5c
Celestinvib <celestinvib@gmail.com>
2022-08-20 12:08:05 +0200

    deploy-heroku-ex4 v1.0

--- TA25_Exercise2/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into departments (name,budget) values('RRHH',30000);
-insert into departments (name,budget) values('Testing',40000);
-insert into departments (name,budget) values('Marketing',22000);
-
-insert into employees (DNI,name,surnames,dpt_code) values('3775629A','Mike','Wasoski Jimenez',1);
-insert into employees (DNI,name,surnames,dpt_code) values('3652126F','Logan','Paul',1);
--- TA25_Exercise3/src/main/java/com/crud/h2/dao/IBoxDAO.java
+++
@@ -1,9 +0,0 @@
-package com.crud.h2.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crud.h2.dto.Box;
-
-public interface IBoxDAO extends JpaRepository<Box, Long>{
-
-}
